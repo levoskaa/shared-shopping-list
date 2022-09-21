759d4c1d46bb479e4a4df4da9df962665c767253
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-09-21 13:51:00 +0200

    feat: add Autofac and AutoMapper

---
+++ SharedShoppingList.API/Infrastructure/SharedShoppingListModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace SharedShoppingList.API.Infrastructure
+{
+    public class SharedShoppingListModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // TODO
+        }
+    }
+}
--- SharedShoppingList.API/Program.cs
+++ SharedShoppingList.API/Program.cs
@@ -1,19 +1,37 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper.Contrib.Autofac.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SharedShoppingList.API.Data;
+using SharedShoppingList.API.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Add secrets to configuration object
 builder.Host.ConfigureAppConfiguration((context, config) =>
     config.AddJsonFile("secrets.json", false, true));
 
 #region Add services to the container.
+// Autofac configuration
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        // AutoMapper
+        builder.RegisterAutoMapper(typeof(Program).Assembly);
+        // Custom types
+        builder.RegisterModule(new SharedShoppingListModule());
+    });
+
+// Controllers
 builder.Services.AddControllers();
+
 // Swagger - more about configuration at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 // DbContext
 builder.Services.AddDbContext<SharedShoppingListContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 #endregion
 
 var app = builder.Build();
