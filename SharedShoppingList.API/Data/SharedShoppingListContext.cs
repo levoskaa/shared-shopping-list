071c8779319fe1b2348b821a9de5cc707dfaa1a5
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-09-21 12:38:35 +0200

    feat: add EF Core

---
+++ SharedShoppingList.API/Data/SharedShoppingListContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SharedShoppingList.API.Data
+{
+    public class SharedShoppingListContext : DbContext
+    {
+        public SharedShoppingListContext(DbContextOptions<SharedShoppingListContext> options)
+            : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ApplyEntityConfigurations(modelBuilder);
+        }
+
+        private static void ApplyEntityConfigurations(ModelBuilder modelBuilder)
+        {
+            // TODO
+        }
+    }
+}
--- SharedShoppingList.API/Program.cs
+++ SharedShoppingList.API/Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SharedShoppingList.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+// Add secrets to configuration object
+builder.Host.ConfigureAppConfiguration((context, config) =>
+    config.AddJsonFile("secrets.json", false, true));
 
-// Add services to the container.
-
+#region Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Swagger - more about configuration at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// DbContext
+builder.Services.AddDbContext<SharedShoppingListContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,3 +32,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
