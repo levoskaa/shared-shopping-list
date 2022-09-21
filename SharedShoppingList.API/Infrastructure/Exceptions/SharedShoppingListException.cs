8813395925498a5353b64e6f51a32f1cfc6cac18
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-09-21 16:07:11 +0200

    feat: add command validation

---
+++ SharedShoppingList.API/Infrastructure/Exceptions/SharedShoppingListException.cs
@@ -0,0 +1,22 @@
+﻿namespace SharedShoppingList.API.Infrastructure.Exceptions
+{
+    public abstract class SharedShoppingListException : Exception
+    {
+        public IEnumerable<string> ErrorCodes { get; set; } = Array.Empty<string>();
+
+        public SharedShoppingListException()
+        {        
+        }
+
+        public SharedShoppingListException(string message)
+            : base(message)
+        {
+        }
+
+        public SharedShoppingListException(string message, params string[] errorCodes)
+            : base(message)
+        {
+            ErrorCodes = errorCodes;
+        }
+    }
+}
--- SharedShoppingList.API/Infrastructure/SharedShoppingListModule.cs
+++ SharedShoppingList.API/Infrastructure/SharedShoppingListModule.cs
@@ -1,12 +1,25 @@
 ﻿using Autofac;
+using FluentValidation;
+using MediatR;
+using SharedShoppingList.API.Application.Behaviors;
+using System.Reflection;
 
 namespace SharedShoppingList.API.Infrastructure
 {
-    public class SharedShoppingListModule : Module
+    public class SharedShoppingListModule : Autofac.Module
     {
         protected override void Load(ContainerBuilder builder)
         {
-            // TODO
+            // Validators
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(type => type.IsClosedTypeOf(typeof(IValidator<>)))
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+
+            // Behaviors
+            builder.RegisterGeneric(typeof(ValidatorBehavior<,>))
+                .As(typeof(IPipelineBehavior<,>))
+                .InstancePerDependency();
         }
     }
 }
