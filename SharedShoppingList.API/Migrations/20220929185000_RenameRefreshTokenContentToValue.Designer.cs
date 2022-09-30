9958732784c6679e8772a27975971c511236ab0a
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-09-30 16:08:57 +0200

    feat: add migration for renaming refresh token content property

--- SharedShoppingList.API/Migrations/20220929185000_RenameRefreshTokenContentToValue.Designer.cs
+++ SharedShoppingList.API/Migrations/20220929185000_RenameRefreshTokenContentToValue.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SharedShoppingList.API.Data;
 
@@ -11,9 +12,10 @@ using SharedShoppingList.API.Data;
 namespace SharedShoppingList.API.Migrations
 {
     [DbContext(typeof(SharedShoppingListContext))]
-    partial class SharedShoppingListContextModelSnapshot : ModelSnapshot
+    [Migration("20220929185000_RenameRefreshTokenContentToValue")]
+    partial class RenameRefreshTokenContentToValue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
