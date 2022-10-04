464e9c247774eda2573c90d0ef223e5fa4c50295
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-10-04 16:53:28 +0200

    feat: create migration for adding owner to UserGroup

---
+++ SharedShoppingList.API/Migrations/20221004144600_AddOwnerToUserGroups.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharedShoppingList.API.Migrations
+{
+    public partial class AddOwnerToUserGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "UserGroups",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroups_OwnerId",
+                table: "UserGroups",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroups_AspNetUsers_OwnerId",
+                table: "UserGroups",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroups_AspNetUsers_OwnerId",
+                table: "UserGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserGroups_OwnerId",
+                table: "UserGroups");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "UserGroups");
+        }
+    }
+}
