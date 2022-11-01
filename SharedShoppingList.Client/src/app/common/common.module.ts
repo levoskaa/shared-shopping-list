b0c22fbed9dee3433b9fa950f6b5f6439a47b9bb
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-11-01 13:25:39 +0100

    fix: import neede UI modules

--- SharedShoppingList.Client/src/app/common/common.module.ts
+++ SharedShoppingList.Client/src/app/common/common.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
 
-const uiModules = [CheckboxModule];
+const uiModules = [CheckboxModule, InputTextModule, ButtonModule, RippleModule];
 
 @NgModule({
   declarations: [],
