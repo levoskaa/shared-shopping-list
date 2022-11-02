78c8f00d914edb0a79d59362baae7cb9461b72af
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-11-02 11:16:44 +0100

    feat(client): add generic HTTP client

---
+++ SharedShoppingList.Client/src/app/core/http/app-http-client.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppHttpClient {
+  constructor(private readonly httpClient: HttpClient) {}
+
+  post<TResponse, TBody>(url: string, body: TBody): Observable<TResponse> {
+    return this.httpClient.post<TResponse>(
+      `${environment.apiBaseUrl}/${url}`,
+      body
+    );
+  }
+
+  get<TResponse, TParams>(
+    url: string,
+    params?: TParams
+  ): Observable<TResponse> {
+    const paramsWithoutNils = JSON.parse(JSON.stringify(params));
+    return this.httpClient.get<TResponse>(`${environment.apiBaseUrl}/${url}`, {
+      params: paramsWithoutNils,
+    });
+  }
+
+  put<TResponse, TBody>(url: string, body: TBody): Observable<TResponse> {
+    return this.httpClient.put<TResponse>(
+      `${environment.apiBaseUrl}/${url}`,
+      body
+    );
+  }
+
+  delete<TResponse>(url: string): Observable<TResponse> {
+    return this.httpClient.delete<TResponse>(
+      `${environment.apiBaseUrl}/${url}`
+    );
+  }
+}
