544a5c60425c9f82e9434fadee7849522814e982
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-11-08 11:05:52 +0100

    feat(client): create header component

---
+++ SharedShoppingList.Client/src/app/shared/common-ui/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  items!: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      { label: 'New', icon: 'pi pi-fw pi-plus' },
+      { label: 'Open', icon: 'pi pi-fw pi-download' },
+      { label: 'Undo', icon: 'pi pi-fw pi-refresh' },
+    ];
+  }
+}
