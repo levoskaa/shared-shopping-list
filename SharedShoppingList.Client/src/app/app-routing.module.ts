2a83f55c33491657e6f211d13dcdee60fb763f69
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-11-08 11:30:47 +0100

    feat(client): add layout component

--- SharedShoppingList.Client/src/app/app-routing.module.ts
+++ SharedShoppingList.Client/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './shared/common-ui/layout/layout.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [],
   },
 ];
 
--- SharedShoppingList.Client/src/app/shared/shared.module.ts
+++ SharedShoppingList.Client/src/app/shared/shared.module.ts
@@ -1,19 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { InputTextModule } from 'primeng/inputtext';
+import { MenuModule } from 'primeng/menu';
 import { RippleModule } from 'primeng/ripple';
+import { HeaderComponent } from './common-ui/header/header.component';
+import { LayoutComponent } from './common-ui/layout/layout.component';
 import { LogoComponent } from './common-ui/logo/logo.component';
 
-const uiModules = [CheckboxModule, InputTextModule, ButtonModule, RippleModule];
+const uiModules = [
+  CheckboxModule,
+  InputTextModule,
+  ButtonModule,
+  RippleModule,
+  MenuModule,
+];
 
-const components = [LogoComponent];
+const components = [LayoutComponent, HeaderComponent, LogoComponent];
 
 @NgModule({
   declarations: [...components],
-  imports: [...uiModules],
+  imports: [RouterModule, TranslateModule, ...uiModules],
   exports: [CommonModule, TranslateModule, ...uiModules, ...components],
 })
 export class SharedModule {}
