50de49e75d48fc460433a23a9a631b094876c7ae
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-11-13 19:12:31 +0100

    feat(client): implement authorized guard

--- SharedShoppingList.Client/src/app/app-routing.module.ts
+++ SharedShoppingList.Client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInPageComponent } from './components/sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
+import { AuthorizedGuard } from './core/guards/authorized.guard';
 import { UnauthorizedGuard } from './core/guards/unauthorized.guard';
+import { LayoutComponent } from './shared/common-ui/layout/layout.component';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: 'sign-in',
     component: SignInPageComponent,
     canActivate: [UnauthorizedGuard],
   },
@@ -14,6 +16,11 @@ const routes: Routes = [
     path: 'sign-up',
     component: SignUpPageComponent,
     canActivate: [UnauthorizedGuard],
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivateChild: [AuthorizedGuard],
   },
 ];
 
