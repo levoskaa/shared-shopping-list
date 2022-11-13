242429b8624e45286ca8c5758a2b317beb702394
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-11-13 15:52:25 +0100

    fix(client): fix JWT expiry time extraction

--- SharedShoppingList.Client/src/app/core/utils/getTokenExpiryTime.ts
+++ SharedShoppingList.Client/src/app/core/utils/getTokenExpiryTime.ts
@@ -2,5 +2,7 @@ import { decodeJwt } from './decodeJwt';
 
 export function getTokenExpiryTime(token: string): Date {
   const decodedJwt = decodeJwt(token);
-  return new Date(decodedJwt['exp']);
+  const secondsSinceEpoch = parseInt(decodedJwt['exp']);
+  // JavaScript Date needs milliseconds, JWT expiry is defined in seconds.
+  return new Date(secondsSinceEpoch * 1000);
 }
