15eac1dcf607ef7f068caa826f89181c0839aa2d
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-11-15 17:51:22 +0100

    fix(client): check text length in capitalizeFirst

--- SharedShoppingList.Client/src/app/core/services/text-transform.service.ts
+++ SharedShoppingList.Client/src/app/core/services/text-transform.service.ts
@@ -5,6 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class TextTransformService {
   capitalizeFirst(text: string): string {
+    if (text.length < 1) {
+      return '';
+    }
     return `${text[0].toUpperCase()}${text.slice(1)}`;
   }
 }
